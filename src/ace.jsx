285e8670b31789d7ce90eea437f14cbfc406f9dd
Jose Galisteo <ceritium@gmail.com>
2017-04-13 23:43:18 +0200

    Use prop-type package instead of React.PropType
    In 15.5, instead of accessing PropTypes from the main React object,
    install the prop-types package and import them from there.
    
    https://facebook.github.io/react/blog/2017/04/07/react-v15.5.0.html#migrating-from-react.proptypes

--- src/ace.jsx
+++ src/ace.jsx
@@ -1,5 +1,6 @@
 import ace from 'brace';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import isEqual from 'lodash.isequal';
 
 const { Range } = ace.acequire('ace/range');
