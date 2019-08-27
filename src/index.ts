83532351deee1df9155054630edbc71faa7c4b2b
James Hrisho <james.hrisho@gmail.com>
2019-08-26 20:20:32 -0400

    Export types (#670)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import ace from "./ace";
-import diff from "./diff";
-import split from "./split";
-export { split, diff };
+import ace, { IAceEditorProps } from "./ace";
+import diff, { IDiffEditorProps, IDiffEditorState } from "./diff";
+import split, { ISplitEditorProps } from "./split";
+import {
+  IAceOptions,
+  ICommand,
+  ICommandBindKey,
+  ICommandManager,
+  IEditorProps,
+  IMarker
+} from "./types";
+export {
+  split,
+  diff,
+  IAceOptions,
+  IAceEditorProps,
+  ICommand,
+  ICommandBindKey,
+  ICommandManager,
+  IDiffEditorProps,
+  IDiffEditorState,
+  IEditorProps,
+  IMarker,
+  ISplitEditorProps
+};
 export default ace;
