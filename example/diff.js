5cbde2bb466cd8eb2eb5b4bd4fa8186aaec06f64
James Hrisho <james.hrisho@gmail.com>
2024-03-23 12:43:24 -0400

    Fix examples for react 18

--- example/diff.js
+++ example/diff.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { diff as DiffEditor } from "../src/index";
 
 import "ace-builds/src-noconflict/mode-jsx";
@@ -106,5 +106,5 @@ class App extends Component {
     );
   }
 }
-
-render(<App />, document.getElementById("example"));
+const root = createRoot(document.getElementById("example"));
+root.render(<App />);
